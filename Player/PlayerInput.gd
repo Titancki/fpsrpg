9bbd5a38048b8c42e139236c4a97d0ce6b2e37a3
Titancki <ouf.guillaume@gmail.com>
2024-02-12 11:43:32 +0100

    removing artefacts

--- Player/PlayerInput.gd
+++
@@ -1,7 +0,0 @@
-# player_input.gd
-extends MultiplayerSynchronizer
-
-func _ready():
-	# Only process for the local player.
-	set_process(get_multiplayer_authority() == multiplayer.get_unique_id())
-
